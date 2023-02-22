8678b42cd89484622fb0c8a7ac421567e5e9ed0f
elahemortazavi <emor5680@gmail.com>
2023-02-22 01:50:44 +0000

    css

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
+import "./index.css";
+import Banner from "./Components/Banner";
 
 class App extends Component {
   render() {
     return (
         <div>
           <Navbar />
+          <Banner />
 
         </div>
     );
