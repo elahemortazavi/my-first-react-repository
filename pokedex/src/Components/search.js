d2bef9d9a0ee0698fdf0f4408aaf2dbd6ec925bc
elahemortazavi <emor5680@gmail.com>
2023-02-27 00:38:10 +0000

    search

---
+++ pokedex/src/Components/search.js
@@ -0,0 +1,13 @@
+import React, { Component } from "reac";
+
+class search extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default search;
