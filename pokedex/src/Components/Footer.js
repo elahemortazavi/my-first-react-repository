e8bee7ad2722beb663c36f28706c422ab7248e46
elahemortazavi <emor5680@gmail.com>
2023-02-27 01:57:50 +0000

    css

--- pokedex/src/Components/Footer.js
+++ pokedex/src/Components/Footer.js
@@ -2,12 +2,11 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <div className = {styles.container} >
-        <p>&copy; all rights reserved 2023</p>
-            
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <p>&copy; all rights reserved 2023</p>
+    </div>
+  );
 };
 
 export default Footer;
