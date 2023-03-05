a6ac27de7f22fe556de39a0fa1b7f4f41d8ace7f
elahemortazavi <emor5680@gmail.com>
2023-03-05 13:12:32 +0000

    list rendering

--- pokedex/src/Components/Cards.js
+++ pokedex/src/Components/Cards.js
@@ -5,17 +5,18 @@ import flowerA from "../images/flowerA.jpg";
 import flowerB from "../images/flowerB.jpg";
 import flowerC from "../images/flowerC.jpg";
 import flowerD from "../images/flowerD.jpg";
+import {v4} from "uuid";
 
 
 class Cards extends Component {
  constructor(){
   super();
-  this.state= {
+  this.state = {
     flowerData: [
-      { image:flowerA, name:"flowerA", cost:"20 $" },
-      { image:flowerB, name:"flowerB", cost:"25 $" },
-      { image:flowerC, name:"flowerC", cost:"40 $" },
-      { image:flowerD, name:"flowerD", cost:"30 $" },
+      { id: 1, image: flowerA, name: "flowerA", cost: "20 $" },
+      { id: 2, image: flowerB, name: "flowerB", cost: "25 $" },
+      { id: 3, image: flowerC, name: "flowerC", cost: "40 $" },
+      { id: 4, image: flowerD, name: "flowerD", cost: "30 $" },
     ],
   };
  }
@@ -27,7 +28,7 @@ class Cards extends Component {
             <Card image={flowerB} name="flowerB" cost="25 $" />
             <Card image={flowerC} name="flowerC" cost="40 $" />
             <Card image={flowerD} name="flowerD" cost="30 $" /> */}
-            {this.state.flowerData.map((flower) => <Card image={flower.image} name={flower.name} cost={flower.cost} />)}
+            {this.state.flowerData.map((flower) => <Card key={v4} image={flower.image} name={flower.name} cost={flower.cost} />)}
 
 
           </div>
