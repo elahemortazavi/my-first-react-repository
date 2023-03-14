f27a6c08906321e11cfaabbeb2b6d0c92091ab3f
elahemortazavi <emor5680@gmail.com>
2023-03-14 22:29:50 +0000

    footer

--- pokedex/src/Components/Footer.js
+++ pokedex/src/Components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <p>&copy; all rights reserved 2023</p>
+      <h3>Made with Love by Elahe Mortazavi</h3>
     </div>
   );
 };
