adb05df4a08735d74941a59bfd075a99dbf057d8
elahemortazavi <emor5680@gmail.com>
2023-03-16 21:59:05 +0000

    Update Banner.js

--- pokedex/src/Components/Banner.js
+++ pokedex/src/Components/Banner.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./Banner.module.css";
 import banner from "../images/banner.jpg";
-import tulip from "../images/anni.jpg";
+import blossom from "../images/blossom.jpg";
 
 const Banner = () => {
     return (
         <div className = {styles.container}>
-            <img src = {tulip} alt = "banner"/>
+            <img src = {blossom} alt = "banner"/>
             <div className = {styles.textContainer}>
                 <h1>Botostart</h1>
                 <p>
